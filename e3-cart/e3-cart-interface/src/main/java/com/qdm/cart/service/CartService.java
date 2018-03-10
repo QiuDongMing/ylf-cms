45d509866b8c57cc01ac3c86ea1eda10700f8bb9
qdm <qdm213@qq.com>
2018-03-10 10:01:39 +0800

    【ADD】 添加购物车

---
+++ e3-cart/e3-cart-interface/src/main/java/com/qdm/cart/service/CartService.java
@@ -0,0 +1,15 @@
+package com.qdm.cart.service;
+
+import com.qdm.common.utils.E3Result;
+import com.qdm.pojo.TbItem;
+
+import java.util.List;
+
+public interface CartService {
+
+	E3Result addCart(long userId, long itemId, int num);
+	E3Result mergeCart(long userId, List<TbItem> itemList);
+	List<TbItem> getCartList(long userId);
+	E3Result updateCartNum(long userId, long itemId, int num);
+	E3Result deleteCartItem(long userId, long itemId);
+}
